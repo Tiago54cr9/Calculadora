c776ba77c2f02c0b84c36fcf91574556493cbd5f
Tiago Teixeira <108500810+Tiago54cr9@users.noreply.github.com>
2022-07-08 18:52:19 +0900

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,40 @@
+## Calculadora de soma ##
+
+primeiro_numero = input("Escreva o seu primeiro número:")
+segundo_numero = input("Escreva o seu segundo número:")
+print("O seu resultado é:",int(primeiro_numero) + int(segundo_numero))
+
+
+## Calculadora de subtração ##
+
+primeiro_numero = input("Escreva o seu primeiro número:")
+segundo_numero = input("Escreva o seu segundo número:")
+print("O seu resultado é:",int(primeiro_numero) - int(segundo_numero))
+
+
+## Calculadora de multiplicação ##
+
+primeiro_numero = input("Escreva o seu primeiro número:")
+segundo_numero = input("Escreva o seu segundo número:")
+print("O seu resultado é:",int(primeiro_numero) * int(segundo_numero))
+
+
+## Calculadora de divisão ##
+
+primeiro_numero = input("Escreva o seu primeiro número:")
+segundo_numero = input("Escreva o seu segundo número:")
+print("O seu resultado é:",int(primeiro_numero) / int(segundo_numero))
+
+
+## Calculadora de divisão inteira##
+
+primeiro_numero = input("Escreva o seu primeiro número:")
+segundo_numero = input("Escreva o seu segundo número:")
+print("O seu resultado é:",int(primeiro_numero) // int(segundo_numero))
+
+
+## Calculadora do resto da divisão##
+
+primeiro_numero = input("Escreva o seu primeiro número:")
+segundo_numero = input("Escreva o seu segundo número:")
+print("O seu resultado é:",int(primeiro_numero) % int(segundo_numero))
